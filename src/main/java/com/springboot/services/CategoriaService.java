c35cb45f7ba87133d9a97f47008d4c911a06ed1a
Cleber <luiz.cleber@gmail.com>
2021-10-11 19:02:15 -0300

    video 20

--- src/main/java/com/springboot/services/CategoriaService.java
+++ src/main/java/com/springboot/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.springboot.services;
 
 import com.springboot.domain.Categoria;
 import com.springboot.repositories.CategoriaRepository;
+import com.springboot.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,13 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
+        }
     }
 
 
-}
+
